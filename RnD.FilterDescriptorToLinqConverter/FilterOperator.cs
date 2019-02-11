834faaaeb4fdde18584e3d6d3154cc5d65cf3d2d
Volodymyr Fursenko <v.fursenko@gmail.com>
2019-02-11 10:50:29 +0300

    first commit

---
+++ RnD.FilterDescriptorToLinqConverter/FilterOperator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnD.FilterDescriptorToLinqConverter
+{
+    public enum FilterOperator
+    {
+          IsLike = 0
+        , IsNotLike = 1
+        , IsLessThan = 2
+        , IsLessThanOrEqualTo = 3
+        , IsEqualTo = 4
+        , IsNotEqualTo = 5
+        , IsGreaterThanOrEqualTo = 6
+        , IsGreaterThan = 7
+        , StartsWith = 8
+        , EndsWith = 9
+        , Contains = 10
+        , NotContains = 11
+        , IsNull = 12
+        , IsNotNull = 13
+        , IsContainedIn = 14
+        , IsNotContainedIn = 15
+    }
+}
---
+++ RnD.FilterDescriptorToLinqConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RnD.FilterDescriptorToLinqConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operations = GetOperationList();
+
+            ParameterExpression operationVariableExpression = Expression.Variable(typeof(Operation));
+            MemberExpression memberExpression = Expression.PropertyOrField(operationVariableExpression, "Name");
+            var binarryExpression = Expression.Equal(memberExpression, Expression.Constant("Operation_10"));
+            var lyamdaExpression = Expression.Lambda<Func<Operation, bool>>(binarryExpression, operationVariableExpression);
+            var filter = lyamdaExpression.Compile();
+
+            var result = operations.Where(filter);
+
+            Console.ReadKey();
+        }
+
+        public static List<Operation> GetOperationList()
+        {
+            var result = new List<Operation>();
+
+            for (int i = 1; i < 1000; i++)
+            {
+                result.Add(new Operation()
+                {
+                    Id = i
+                    , Name = String.Format("Operation_{0}", i)
+                    , Description = String.Format("Description_{0}", i)
+                });
+            }
+
+            return result;
+        }
+    }
+}
